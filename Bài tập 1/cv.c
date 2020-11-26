d2aa3e80ff0724809964e898ea81da98ec3e2090
Trương Trần Trung Nam <72543365+0793559976@users.noreply.github.com>
2020-11-26 17:00:31 +0700

    Create cv.c

---
+++ Bài tập 1/cv.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+// Khai bao bien R  kieu double
+double R ;
+// khoi tao bien R = 2.5 
+R = 2.5 ;
+const float PI = 3.14 ;
+// khai bao cac bien CV,DT kieu double
+double CV,DT ;
+// khai bao CV = 2*R*PI 
+CV = 2*R*PI ;
+// khai bao DT =  R*R*PI
+DT = R*R*PI ;
+printf ("CV\t DT\n %0.1f  %0.1f",CV,DT);
+
+printf ("\n");
+
+
+
+
+
+getchar();
+return 0;
+}  
