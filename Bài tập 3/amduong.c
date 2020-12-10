8f58448ecd68e7286f61d897625652e026287e36
Trương Trần Trung Nam <72543365+0793559976@users.noreply.github.com>
2020-12-10 15:08:52 +0700

    Update amduong.c

--- Bài tập 3/amduong.c
+++ Bài tập 3/amduong.c
@@ -4,10 +4,13 @@ int main(){
 	int a = -1;
 	if (a > 0){
 		printf ("%d duong", a);
+		scanf ("%d", &a);
 	}else if (a == 0) {
 		printf ("a = 0", a);
+		scanf ("%d", &a);
 	}else {
 		printf ("%d am", a);
+		scanf ("%d", &a);
 	}
 
 	
