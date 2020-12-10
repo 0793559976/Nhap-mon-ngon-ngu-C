87a8f5238f5db7ba363303e83f9832dff4f38aec
Trương Trần Trung Nam <72543365+0793559976@users.noreply.github.com>
2020-12-10 16:35:22 +0700

    Add files via upload

---
+++ Bài tập 3/hoadon.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+	// khai bao bien don gia,so luong
+	int dongia,soluong,thanhtien;
+	printf ("dongia =");
+	scanf("%d",& dongia) ;
+	printf ("soluong =");
+	scanf("%d",& soluong);
+	thanhtien = soluong*dongia;
+	// dieu kien
+	if (soluong >= 5){ thanhtien= (soluong*dongia)*0.75;
+		printf ("%d",thanhtien );
+		
+	
+		
+	}else {
+		printf (" %d",thanhtien);
+	
+	}
+
+	
+  getchar();
+  return 0;
+}
