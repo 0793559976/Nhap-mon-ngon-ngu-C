c60eb8d8bb31df3d713ebc3253e424c21f9aef53
Trương Trần Trung Nam <72543365+0793559976@users.noreply.github.com>
2020-12-17 17:05:23 +0700

    Create thang2.c

---
+++ Bài tập 3/thang2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+  //khai bao bien a va b
+int thang;
+
+printf("vui long nhap thang:");
+scanf("%d",&thang);
+switch(thang)
+    {   case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            printf("31 ngay");
+            break;
+        case 2:
+            printf("28 ngay");
+            break;
+
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            printf("30 ngay");
+            break;
+        
+        
+         default:
+         printf ("chi co 12 thang trong nam");
+    }
+
+ getchar();
+  return 0;
+}
