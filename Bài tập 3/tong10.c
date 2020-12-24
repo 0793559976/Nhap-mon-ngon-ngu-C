a331c710a980e11df23040dafaa7847838c4c69b
Trương Trần Trung Nam <72543365+0793559976@users.noreply.github.com>
2020-12-24 15:32:38 +0700

    Create tong10.c

---
+++ Bài tập 3/tong10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  
+int tong = 0;
+
+for (int dem = 1; dem <= 10; dem++)
+tong = tong + dem;
+  {
+    printf("tong la:%d ", tong);
+  }
+ getchar();
+  return 0;
+}
