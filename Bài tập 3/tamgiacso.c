519eb1b6a538729e259bfe37234dca5edd26e8dc
Trương Trần Trung Nam <72543365+0793559976@users.noreply.github.com>
2020-12-30 07:16:21 +0700

    Create tamgiacso.c

---
+++ Bài tập 3/tamgiacso.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+	//khai bao nhieu bien
+	int n,a,b;
+	printf("\n\n");
+	printf("Nhap n = ");
+	scanf("%d",& n);
+	printf("n = %d\n",n);
+	for(a = 1; a <= n; a++ ){
+		printf("\n");
+	  for(b = 1; b <= a; b++)//vong lap ben trong
+		  printf("%d",b);
+	}
+	getchar();
+	return 0;
+}
