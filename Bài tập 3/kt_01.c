22eef4667cfdea81763b65d1f7f01d4a31095aa5
Trương Trần Trung Nam <72543365+0793559976@users.noreply.github.com>
2021-01-07 16:12:52 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -3,12 +3,8 @@
 int main(){
 	int a,b;
   
-	printf("Bang cuu chuong\n");
-  
-	for (a=1;a<=9;a++){
-		printf("Bang cuu chuong %d\n",a);
-		for (b=1;b<=9;b++){
-			printf("%d * %d = %d\n",a,b,a*b);
-		}
-	}
+	printf("Bang Cuu Chuong:\n\n");
+  printf("Bang nhan 2:\n2*1=2\n2*2=4\n2*3=6\n\n");
+  printf("Bang nhan 3:\n3*1=3\n3*2=6\n3*3=9\n\n");
+  printf("Bang nhan 4:\n4*1=4\n4*2=8\n4*3=12\n\n");
 }
