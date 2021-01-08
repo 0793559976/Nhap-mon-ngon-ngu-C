fc3181c3a5d4ce1b3f54c36931475ee67d956837
Trương Trần Trung Nam <72543365+0793559976@users.noreply.github.com>
2021-01-08 13:42:17 +0700

    Update kt_03.c

--- Bài tập 3/kt_03.c
+++ Bài tập 3/kt_03.c
@@ -19,3 +19,38 @@ for(a=1;a<=20;a++){
 
   return 0;
 }
+
+
+=============================================================================================
+
+
+#include <stdio.h>
+
+int main(){
+int a=0;
+printf("Viết chương trình in ra các số lẻ từ 1 đến 20 ngoại trừ các số 5, 11, 15 :\n");
+while(a<=20){
+  a++;
+  if(a%2){
+    switch (a){
+      case 5:
+      case 11:
+      case 15:
+      case 21:
+        break;
+      default:
+        printf ("\n%d",a);
+    }
+  }
+  
+}
+
+  return 0;
+}
+
+
+
+
+
+
+
