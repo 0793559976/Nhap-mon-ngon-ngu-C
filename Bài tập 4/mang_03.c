30dff22de113395e01148b34bbba0461cc748063
Trương Trần Trung Nam <72543365+0793559976@users.noreply.github.com>
2021-01-21 13:58:41 +0700

    Update mang_03.c

--- Bài tập 4/mang_03.c
+++ Bài tập 4/mang_03.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 int main(void) {
-  int n,so_chan=0,so_le=0,so_0=0;
+  int n,Sochan=0,Sole=0,So_0=0;
   //nhap so phan tu cua mang a
-  printf("nhap n= ");
+  printf("Nhap n = ");
   scanf("%d",&n);
   int a[n];
 
   for(int i=0;i < n ;i++){
-    printf("nhap phan tu thu %d: ",i+1);
+    printf("Nhap phan tu thu %d: ",i+1);
         scanf("%d", &a[i]);
   }
-    printf("mang a: ");
+    printf("Mang a: ");
   for(int i=0; i < n; i+=1){
       printf(" %d",a[i]);
       }
-  printf("\nso chan tu mang a: ");
+  printf("\nSo chan tu mang a: ");
         //vong lap tinh so chan cua mang a
   for(int i=0;i<n;i++){
         if(a[i] % 2==0 &&a[i]>0){
         printf("%d ",a[i]);
-        so_chan+=1;
+        Sochan+=1;
         }
   }
         printf("\nso le tu mang a: ");
@@ -28,16 +28,16 @@ int main(void) {
   for(int i=0;i<n;i++){
         if(a[i] % 2!=0){
         printf("%d ",a[i]);
-        so_le+=1;
+        Sole+=1;
     }
     } 
     //vong lap tinh so luong so 0   
   for(int i=0;i<n;i++){
         if(a[i] ==0){
-        so_0+=1;
+        So_0+=1;
     }
   }
-  printf("\nso chan :%d \nso le : %d \nso 0:%d",so_chan,so_le,so_0);
+  printf("\nSo chan :%d \nSo le : %d \nSo 0:%d",Sochan,Sole,So_0);
   
   return 0;
 }
